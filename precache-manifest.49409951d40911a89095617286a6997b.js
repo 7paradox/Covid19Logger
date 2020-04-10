7cc6fb145070a87e04a60d2331ccfe38dfcb7373
7paradox <31220459+7paradox@users.noreply.github.com>
2020-04-10 19:53:42 +0530

    Updates

---
+++ precache-manifest.49409951d40911a89095617286a6997b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87f37cbfc0d0b624fb3c353b2bd6984",
+    "url": "/Covid19Logger/index.html"
+  },
+  {
+    "revision": "0d6e57c773b3e9c1c7d1",
+    "url": "/Covid19Logger/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e53c4b8f4d6f455f06b2",
+    "url": "/Covid19Logger/static/css/main.265efe02.chunk.css"
+  },
+  {
+    "revision": "0d6e57c773b3e9c1c7d1",
+    "url": "/Covid19Logger/static/js/2.9074d5e0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid19Logger/static/js/2.9074d5e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53c4b8f4d6f455f06b2",
+    "url": "/Covid19Logger/static/js/main.b7395be4.chunk.js"
+  },
+  {
+    "revision": "786d67a316a91c088eff",
+    "url": "/Covid19Logger/static/js/runtime-main.61014289.js"
+  }
+]);
